922414475274c1e90dc1101284109359f850559f
Vinicius Tiesse <contato@deltacoding.xyz>
2018-01-05 17:43:20 -0200

    changed details button name.

--- src/components/PostList/PostItem/index.js
+++ src/components/PostList/PostItem/index.js
@@ -20,7 +20,7 @@ const Post = ({title, author, comments, score, body}) => (
         {/*Resume of body, if has more than 140 characters trim text and add ...*/}
         <p>
           {stringTruncate(body, 140)}&nbsp;
-          <a className="PostItem-Link" href="#">Ver postagem</a>
+          <a className="PostItem-Link" href="#">Ver detalhes</a>
         </p>
       </CardText>
       <CardActions>
